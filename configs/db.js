574d2f0f86196c52ec80d2265de36e4ee5ce78da
Sumit Kumar Dey Sarkar <skumarcbr4@gmail.com>
2023-10-05 22:44:40 +0530

    error mongo solve

--- configs/db.js
+++ configs/db.js
@@ -1,8 +1,9 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
 
+
 const connect=async ()=>{
-    return mongoose.connect(process.env.mongo_url,{
+    return mongoose.connect(process.env.MONGO_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
     }).then(console.log("connected to mongodb")).catch(err=>console.log(err))
